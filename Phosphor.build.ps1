9843cc646f6ff96b633d81f8c0f595a248632845
EbruCucen <ebrugul@hotmail.com>
2017-05-11 14:18:19 +0100

    npm installs on appveyor

--- Phosphor.build.ps1
+++ Phosphor.build.ps1
@@ -111,14 +111,8 @@ task BuildModule {
 task BuildElectron {
     # TODO: Build the Electron binaries here
 }
-task BuildClient {
-    Push-Location ./src/Phosphor.Client
-    exec { & npm install }
-    exec { & npm run tsc }
-    Pop-Location
-}
 
-task Build BuildModule, BuildClient
+task Build BuildModule
 
 task Test {
     # TODO: Add tests
